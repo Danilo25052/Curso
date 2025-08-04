09d689ad98a2462e3109e05baf32273fa33145e8
Danilo25052 <josedanilobackups@gmail.com>
2025-08-04 14:10:23 -0300

    Update aula01.py

--- Orientação a Objetos/aula_python/aula01.py
+++ Orientação a Objetos/aula_python/aula01.py
@@ -1,1 +1,13 @@
 print("Olá, Mundo!!!")
+
+nome = "Danilo"
+altura = 1.81
+idade = 16
+
+print("Meu nome é", nome, "tenho", idade, "anos e minha altura é", altura, "metros.")
+
+numero1 = 12
+numero2 = 10
+media = (numero1 + numero2) / 2
+
+print("A media de", numero1, "e", numero2, "é", media, ".")
