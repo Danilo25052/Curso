241a002cef76b59c4a979ee73f196af7f750dcd6
Danilo25052 <josedanilobackups@gmail.com>
2025-08-05 11:11:37 -0300

    Aula 02.py

---
+++ Orientação a Objetos/aula_python/aula02.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print("Seu nome é:", nome)
+print("Sua idade é:", idade)
+print("Sua altura é:", altura)
+
+print("Seu nome é:", type(nome))
+print("Sua idade é:", type(idade))
+print("Sua altur é:", type(altura))
+
+numero1 = 12
+numero2 = 10
+media = (numero1 + numero2) / 2
+
+print("A media de", numero1, "e", numero2, "é", media, ".")
