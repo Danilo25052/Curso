74ecb018f2e806a9e88a7b9d2a2d4d414fb4d92a
Danilo25052 <josedanilobackups@gmail.com>
2025-08-06 08:55:35 -0300

    Projeto MoNA/MoCH

---
+++ Orientação a Objetos/aula_python/aula03.py
@@ -0,0 +1,12 @@
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+
+print("A soma dos números é:", a + b)
+print("A subtração dos números é:", a - b)
+
+if b == 0:
+    print("A divisão dos números é: indefinida")
+else:
+    print("A divisão dos números é:", a / b)
+
+print("A multiplicação dos números é:", a * b)
